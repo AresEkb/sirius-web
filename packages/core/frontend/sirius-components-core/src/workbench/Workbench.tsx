59a44dcdd2ed73e8449d7eed9a5004945c1efaaa
Denis Nikiforov <denis.nikif@gmail.com>
2024-11-28 17:19:23 +0300

    Changed panel size in the workspace

--- packages/core/frontend/sirius-components-core/src/workbench/Workbench.tsx
+++ packages/core/frontend/sirius-components-core/src/workbench/Workbench.tsx
@@ -196,9 +196,9 @@ export const Workbench = ({
         editingContextId={editingContextId}
         readOnly={readOnly}
         leftContributions={workbenchViewLeftSideContributions}
-        leftPanelInitialSize={400}
+        leftPanelInitialSize={300}
         rightContributions={workbenchViewRightSideContributions}
-        rightPanelInitialSize={450}
+        rightPanelInitialSize={300}
         mainArea={main}
       />
       <Toast
