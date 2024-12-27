b55558379880012c8767fff37424a292e9da009b
Denis Nikiforov <denis.nikif@gmail.com>
2024-12-27 13:52:21 +0300

    Add extra exports for fronted packages

--- packages/diagrams/frontend/sirius-components-diagrams/src/index.ts
+++ packages/diagrams/frontend/sirius-components-diagrams/src/index.ts
@@ -72,3 +72,62 @@ export type { DiagramPanelActionProps } from './renderer/panel/DiagramPanel.type
 export { diagramPanelActionExtensionPoint } from './renderer/panel/DiagramPanelExtensionPoints';
 export { DiagramRepresentation } from './representation/DiagramRepresentation';
 export type { GQLDiagramDescription } from './representation/DiagramRepresentation.types';
+
+// Metamodel
+export { DiagramDescriptionContext } from './contexts/DiagramDescriptionContext';
+export { useDiagramDescription } from './contexts/useDiagramDescription';
+export { convertDiagram } from './converter/convertDiagram';
+export { DialogContextProvider } from './dialog/DialogContext';
+export { diagramEventSubscription } from './graphql/subscription/diagramEventSubscription';
+export { PinIcon } from './icons/PinIcon';
+export { UnpinIcon } from './icons/UnpinIcon';
+export { useAdjustSize } from './renderer/adjust-size/useAdjustSize';
+export { useBorderChange } from './renderer/border/useBorderChange';
+export { ConnectorContextProvider } from './renderer/connector/ConnectorContext';
+export { ConnectorContextualMenu } from './renderer/connector/ConnectorContextualMenu';
+export { useConnector } from './renderer/connector/useConnector';
+export { DebugPanel } from './renderer/debug/DebugPanel';
+export { useDiagramDelete } from './renderer/delete/useDiagramDelete';
+export { DiagramDirectEditContextProvider } from './renderer/direct-edit/DiagramDirectEditContext';
+export { useNodesDraggable } from './renderer/drag/useNodesDraggable';
+export { DropNodeContextProvider } from './renderer/dropNode/DropNodeContext';
+export { useDropDiagramStyle } from './renderer/dropNode/useDropDiagramStyle';
+export { ConnectionLine } from './renderer/edge/ConnectionLine';
+export { MarkerDefinitions } from './renderer/edge/MarkerDefinitions';
+export { useEdgeType } from './renderer/edge/useEdgeType';
+export { useEditableEdgePath } from './renderer/edge/useEditableEdgePath';
+export { useFadeDiagramElements } from './renderer/fade/useFadeDiagramElements';
+export { useInitialFitToScreen } from './renderer/fit-to-screen/useInitialFitToScreen';
+export { FullscreenContextProvider } from './renderer/fullscreen/FullscreenContext';
+export { useHandleChange } from './renderer/handles/useHandleChange';
+export { HelperLines } from './renderer/helper-lines/HelperLines';
+export { useHelperLines } from './renderer/helper-lines/useHelperLines';
+export { useNodeHover } from './renderer/hover/useNodeHover';
+export { useLayoutOnBoundsChange } from './renderer/layout-events/useLayoutOnBoundsChange';
+export { useSynchronizeLayoutData } from './renderer/layout/useSynchronizeLayoutData';
+export { useMoveChange } from './renderer/move/useMoveChange';
+export { NodeContextProvider } from './renderer/node/NodeContext';
+export { useNodeType } from './renderer/node/useNodeType';
+export {
+  DiagramElementPaletteContext,
+  DiagramElementPaletteContextProvider,
+} from './renderer/palette/contexts/DiagramElementPaletteContext';
+export type { DiagramElementPaletteContextValue } from './renderer/palette/contexts/DiagramElementPaletteContext.types';
+export { DiagramPaletteContextProvider } from './renderer/palette/contexts/DiagramPaletteContext';
+export { DiagramPalette } from './renderer/palette/DiagramPalette';
+export { GroupPalette } from './renderer/palette/group-tool/GroupPalette';
+export { useGroupPalette } from './renderer/palette/group-tool/useGroupPalette';
+export { useDiagramElementPalette } from './renderer/palette/useDiagramElementPalette';
+export { useDiagramPalette } from './renderer/palette/useDiagramPalette';
+export { usePalette } from './renderer/palette/usePalette';
+export { DiagramPanel } from './renderer/panel/DiagramPanel';
+export { usePinDiagramElements } from './renderer/pin/usePinDiagramElements';
+export { useReconnectEdge } from './renderer/reconnect-edge/useReconnectEdge';
+export { useResizeChange } from './renderer/resize/useResizeChange';
+export { useDiagramSelection } from './renderer/selection/useDiagramSelection';
+export { useShiftSelection } from './renderer/selection/useShiftSelection';
+export { useSnapToGrid } from './renderer/snap-to-grid/useSnapToGrid';
+export { StoreContextProvider } from './representation/StoreContext';
+export { useDiagramSubscription } from './representation/useDiagramSubscription';
+export type { UseDiagramSubscriptionValue } from './representation/useDiagramSubscription.types';
+export { useStore } from './representation/useStore';
